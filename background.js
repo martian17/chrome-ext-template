722f0253ec1245de36b0bd02da8b5d5239fda21d
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-09-17 12:29:22 +0900

    deleted some old comments

--- background.js
+++ background.js
@@ -1,11 +1,6 @@
-
 //Loading up the dependencies
 importScripts("chromeext-msg-wrapper/background.port.js");
-/*try {
-    //importScripts("chromeext-msg-wrapper/background.port.js");
-} catch(err){
-    console.error(err);
-}*/
+
 
 
 let app = new Port();
